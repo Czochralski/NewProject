37bf1ac250ded55c4570e05166405e2550bdbf8c
userblb <userblb@UNI002-L003-20.serlab.intra>
2018-03-14 09:45:30 -0300

    primeira versao funcional

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Variaveis 
+            double A;
+            double Resultado;
+           
+
+            //Logica da operação
+            Console.WriteLine("Digite um valor");
+            A = double.Parse(Console.ReadLine());
+
+            Resultado = 0.1 *A;
+           
+            Console.WriteLine(Resultado);
+            Console.ReadLine();
+        }
+    
+
+    static void Segundo(string[] args)
+    {
+
+        //Variaveis 
+        double Porcentagem;
+        double N;
+        double Resultado;
+
+
+    
+            //Logica da operação
+            Console.WriteLine("Digite o valor da porcentagem");
+            Porcentagem = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor da porcentagemdo numero");
+            N = double.Parse(Console.ReadLine());
+
+            Resultado = ((double)Porcentagem/100)*N;
+
+        Console.WriteLine(Resultado);
+        Console.ReadLine();
+    }
+}
+}
