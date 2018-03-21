ca417db1681c42648af0a20746a301a4000bbf6f
userblb <userblb@UNI002-L003-17.serlab.intra>
2018-03-21 10:33:25 -0300

    Maminha projeto

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, x;
+
+            Console.WriteLine("Entre com o valor de A:");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o valor de B:");
+            b = double.Parse(Console.ReadLine());
+
+            x = -b / a;
+
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
