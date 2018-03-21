5ed9201b07e94164b267fea8c787282f2f6f1555
userblb <userblb@UNI002-L003-17.serlab.intra>
2018-03-21 09:55:18 -0300

    Projeto novo

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N;
+            int Resultado_numero;
+            Console.WriteLine("escreva um nuemro desejado");
+            N = int.Parse(Console.ReadLine());
+            Resultado_numero = N % 2;
+            if (Resultado_numero == 1)
+            {
+                Console.WriteLine("Numero impar");
+            }
+            else
+            {
+                Console.WriteLine("Numero è par");
+            };
+            Console.WriteLine(Resultado_numero);
+            Console.ReadLine();
+        }
+}
+}
