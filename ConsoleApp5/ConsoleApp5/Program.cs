e82ff3c0df01418130486202511284acf475428e
Maminha1000 <37370135+Maminha1000@users.noreply.github.com>
2018-04-02 16:16:56 -0300

    Update Program.cs

--- ConsoleApp5/ConsoleApp5/Program.cs
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -10,15 +10,16 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
+            //variáveis
             double a, b, x;
-
+//inserir os valores digitado pelo usuário
             Console.WriteLine("Entre com o valor de A:");
             a = double.Parse(Console.ReadLine());
             Console.WriteLine("Entre com o valor de B:");
             b = double.Parse(Console.ReadLine());
-
+//a lógica da operação
             x = -b / a;
-
+//mostrar o resultado na tela do usuário
             Console.WriteLine(x);
             Console.ReadLine();
         }
